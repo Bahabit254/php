080475494d04ff05562ce9d051228585e54cd0b5
Bahabit254 <bahatiroy@gmail.com>
2023-01-22 19:13:06 +0300

    Inheritance from classes

--- index.php
+++ index.php
@@ -25,18 +25,31 @@
             }
         }
 
+    
+    }
+    
+    class AdminUser extends User {
+        public $level;
+
+        public function __construct($username, $email, $level) {
+            $this->level = $level;
+            parent::__construct($username, $email);
+        }
     }
 
     $userOne = new User('brosnan', 'brosnan@example.com');
     $userTwo = new User('pierce', 'pierce@example.com');
-    $userThree = new User('mario', 'mario@example.com');
+    // $userThree = new User('mario', 'mario@example.com');
+    $userFour = new AdminUser('bronco', 'bronco@example.com', 3);
 
     // echo $userOne->username . '<br>';
     // echo $userOne->email . '<br>'; >command will return error as email is private
     // echo $userOne->addFriend() . '<br>';
-    $userOne->setEmail('bruno@example.com');
+    // $userOne->setEmail('bruno@example.com');
 
-    echo $userOne->getEmail() . '<br';
+    echo $userFour->username . '<br';
+    echo $userFour->getEmail() . '<br';
+    echo $userFour->level . '<br';
 
     // print_r(get_class_vars('User'));
     // print_r(get_class_methods('User'));
